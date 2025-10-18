b09f439698c3fce479948146d89c0c550eede945
sandeepsabu <sandeepsabu321@gmail.com>
2025-10-18 10:34:42 +0530

    added configs

--- src/app/api/chat/list/route.js
+++ src/app/api/chat/list/route.js
@@ -1,7 +1,9 @@
 import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
-import connectDB from "../../../../../config/db";
-import Chat from "../../../../../models/Chat";
+// import connectDB from "../../../../../config/db";
+// import Chat from "../../../../../models/Chat";
+import connectDB from "@/config/db";
+import Chat from "@/models/Chat";
 
 export async function GET(req) {
   try {
